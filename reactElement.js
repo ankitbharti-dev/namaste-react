ab2a80532ea56d23e6c4d70a986ec2c1dba14497
Ankit Bharti <ankitbharti043@gmail.com>
2026-08-15 19:31:44 +0530

    parcel and npm added into git project

--- reactElement.js
+++ reactElement.js
@@ -1,10 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", 
     {id:"div1"}, 
-    [React.createElement("div", {id:"innerDiv1"}, [React.createElement("h1", {id:"header1"}, "Heading 1"), React.createElement("h2", {id:"header2"}, "Heading 2")]),
-[React.createElement("div", {id:"innerDiv2"}, [React.createElement("h1", {id:"header3"}, "Heading 3"), React.createElement("h2", {id:"header4"}, "Heading 4")])]]
+    React.createElement("div", {id:"innerDiv1"}, React.createElement("h1", {id:"header1"}, "Heading 1"), React.createElement("h2", {id:"header2"}, "Heading 2")),
+React.createElement("div", {id:"innerDiv2"}, React.createElement("h1", {id:"header3"}, "Heading 3"), React.createElement("h2", {id:"header4"}, "Heading 4"))
     );
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
