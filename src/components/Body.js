e4cbebb0770fbd0e1a33f168edd1ffc296964b63
Ankit Bharti <ankitbharti043@gmail.com>
2026-08-29 23:58:41 +0530

    restrucuture the code

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import resList from "../utils/mockData";
+import RestaurantCard from "./RestaurantCard";
+
+const Body = () => {
+    return ( <div className='body'>
+        <div className="res-container">
+            {
+                resList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                ))
+            }
+        </div>
+    </div>
+)};
+
+export default Body;
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,23 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        avgRating,
+        deliveryTime,
+        costForTwo
+    } = resData?.data;
+
+    return <div className="res-card">
+        <img className="res-logo" src={CDN_URL+cloudinaryImageId} />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(' ')}</h4>
+        <h4>{avgRating} ⭐ {deliveryTime} minutes</h4>
+        <h4>₹{costForTwo/100} For two</h4>
+    </div>
+}
+
+export default RestaurantCard;
